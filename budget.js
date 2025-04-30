54087aee949683fd1dbc73cbea3a47727fba00a3
aowens04 <cvnfc@iup.edu>
2025-04-30 09:34:22 -0400

    js

---
+++ budget.js
@@ -0,0 +1,17 @@
+window.onload = pageload;
+
+pageload(){
+    document.getElementById("weekly").onClick = weeklyView
+    document.getElementById("monthly").onClick = monthlyView
+    document.getElementById("yearly").onClick = yearlyView
+}
+
+weeklyView(){
+
+}
+monthlyView(){
+  
+}
+yearlyView(){
+    
+}
