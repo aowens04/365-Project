636a44e8afd5b886cca46c73166f62420ffe1fa0
aowens04 <cvnfc@iup.edu>
2025-05-02 15:14:58 -0400

    :(

--- budget.js
+++ budget.js
@@ -1,11 +1,23 @@
 window.onload = pageload;
 
 function pageload(){
-    document.getElementById("add").onClick = update;
+    document.getElementById("add").onclick = update;
 }
 
 function update(){
     weeklyBudget = document.getElementById("input").value;
-    newItem = document.createElement("tr");
-    alert("reached");
+    category = document.getElementById("selCategory").value;
+    amount = document.getElementById("input").value;
+    date = document.getElementById("date");
+
+    tableBody = document.getElementById("table");
+
+    newRow = document.createElement("tr");
+    newCat = document.createElement("td");
+    newAmt = document.createElement("td");
+    newDate = document.createElement("td");
+    newDelete = document.createElement("td");
+
+    tableBody.innerHTML += newRow;
+    tableBody.innerHTML += newCat + newAmt + newDate + newDelete;
 }
