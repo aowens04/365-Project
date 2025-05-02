97c216d9ef7a769f03055e4a53f51bebe4141667
aowens04 <cvnfc@iup.edu>
2025-05-02 16:48:28 -0400

    :(

--- budget.js
+++ budget.js
@@ -1,4 +1,5 @@
 window.onload = pageload;
+totalSpending = Number(0);
 
 function pageload(){
     document.getElementById("add").onclick = update;
@@ -7,17 +8,24 @@ function pageload(){
 function update(){
     weeklyBudget = document.getElementById("input").value;
     category = document.getElementById("selCategory").value;
-    amount = document.getElementById("input").value;
-    date = document.getElementById("date");
+    amount = document.getElementById("amount").value;
+    date = document.getElementById("date").value;
+    total = document.getElementById("total");
 
     tableBody = document.getElementById("table");
+    row = table.insertRow();
+    cell0 = row.insertCell(0);
+    cell1 = row.insertCell(1);
+    cell2 = row.insertCell(2);
+    cell3 = row.insertCell(3);
 
-    newRow = document.createElement("tr");
-    newCat = document.createElement("td");
-    newAmt = document.createElement("td");
-    newDate = document.createElement("td");
-    newDelete = document.createElement("td");
+    cell0.innerHTML = category;
+    cell1.innerHTML = amount;
+    cell2.innerHTML = date;
+    cell3.innerHTML = "delete";
 
-    tableBody.innerHTML += newRow;
-    tableBody.innerHTML += newCat + newAmt + newDate + newDelete;
+    totalSpending += amount;
+    weeklyBudget -= amount;
+    total.innerHTML = totalSpending;
+    
 }
