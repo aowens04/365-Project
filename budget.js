968a80836bcaaf830d4bcfe5af8de4191d921aeb
aowens04 <cvnfc@iup.edu>
2025-05-05 10:20:48 -0400

    monthly & yearly added but not working

--- budget.js
+++ budget.js
@@ -1,10 +1,15 @@
 window.onload = pageload;
-totalSpending = Number(0);
+totalSpendingWeekly = Number(0);
+totalSpendingMonthly = Number(0);
+totalSpendingYearly = Number(0);
 
 function pageload(){
     $("add").onclick = updateWeekly;
     $("set").onclick = createWeekly;
-
+    $("addMonthly").onclick = createMonthly;
+    $("setMonthly").onclick = updateMonthly;
+    $("addYearly").onclick = createYearly;
+    $("setYearly").onclick = updateYearly;
 }
 
 function createWeekly(){
@@ -33,11 +38,47 @@ function updateWeekly(){
     cell2.innerHTML = day;
     cell3.innerHTML = "delete";
 
-    totalSpending += Number(amount);
-    total.innerHTML = totalSpending;
-    remaining.innerHTML = weeklyBudget - totalSpending;
+    totalSpendingWeekly += Number(amount);
+    total.innerHTML = totalSpendingWeekly;
+    remaining.innerHTML = weeklyBudget - totalSpendingWeekly;
     
     if((weeklyBudget - totalSpending) < 0){
         alert("Warning: You've exceeded your budget.");
     }
 }
+
+function createMonthly(){
+    monthlyBudget = $("input").value;
+    remainingMonthly = $("remainingMonthly");
+    remainingMonthly.innerHTML = monthlyBudget;
+}
+/*
+function updateMonthly(){
+    monthlyBudget = $("input").value;
+    remainingMonthly = $("remaining");
+    category = $("selCategory").value;
+    amount = $("amount").value;
+    month = $("date").value;
+    total = $("total");
+
+    tableBody = document.getElementById("table");
+    row = table.insertRow();
+    cell0 = row.insertCell(0);
+    cell1 = row.insertCell(1);
+    cell2 = row.insertCell(2);
+    cell3 = row.insertCell(3);
+
+    cell0.innerHTML = category;
+    cell1.innerHTML = amount;
+    cell2.innerHTML = date;
+    cell3.innerHTML = "delete";
+
+    totalSpendingMonthly += Number(amount);
+    total.innerHTML = totalSpendingMonthly;
+    remainingMonthly.innerHTML = monthlyBudget - totalSpendingMonthly;
+    
+    if((monthlyBudget - totalSpendingMonthly) < 0){
+        alert("Warning: You've exceeded your budget.");
+    }
+}
+*/
